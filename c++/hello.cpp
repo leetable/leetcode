66547d2748c41f601738f91c0e1b78d50e31e5c4
leetable <qyx0306@sina.com>
2020-12-04 16:55:38 +0800

    c++_env

---
+++ c++/hello.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#include <stdlib.h>
+using namespace std;
+int main(){
+    int a;
+    int b;
+    cin>>a>>b;
+    cout<<a+b<<endl;
+    cout<<"123"<<endl;
+    system("pause");
+    return 0;
+}
